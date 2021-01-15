78e9d8af273c75b8f54edcbacf9adef272ea57ab
Gissart <argyaauliaf@gmail.com>
2021-01-15 18:50:04 +0700

    Initial Commit

---
+++ resources/views/destinasi-populer.blade.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/template.css') }}">
+    <title>Destinasi Populer</title>
+</head>
+
+<body>
+
+    <!-- //* NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/img/logo.png" style="width: 150px;" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto navbar-config text-sedang font-18">
+                    <a class="nav-link pl-4 active" href="#">Galeri<span class="sr-only">(current)</span></a>
+                    <a class="nav-link pl-4" href="#">Kontak</a>
+                    <a class="nav-link pl-4" href="#">Tentang Kami</a>
+                    <a class="nav-link pl-4 profile-icon" href="#" tabindex="-1" aria-disabled="true">
+                        <img src="assets/icon/profile-icon.png" style="width: 24px;" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- //* END NAVBAR -->
+
+    <!-- //*CONTENT -->
+    <div class="container justify-content-center">
+        <section class="row">
+            <section class="col-lg-12 mt-lg-5 mb-lg-5">
+                <p class="judul text-tebal font-32">Destinasi Populer</p>
+            </section>
+        </section>
+        <section class="row pb-xl-5">
+            <section class="col-lg-4">
+                <a href="#" class="ahref-no-decor second-text-color">
+                    <div class="card">
+                        <div class="border-image">
+                            <img src="assets/img/labuan-bajo.png" class="card-img-top" alt="..."></div>
+                        <div class="card-body">
+                            <h5 class="card-title text-sedang">Labuan Bajo</h5>
+                            <div class="pulau">
+                                <p class="card-text text-biasa font-14">Nusa Tenggara Timur</p>
+                            </div>
+                            <p class="text-kecil font-12 mt-4 mb-n1">Mulai dari</p>
+                            <p class="text-tebal mb-0">Rp <span class="warna-harga">600.000</span><span
+                                    class="text-biasa kecil">/org</span>
+                            </p>
+                            <div class="rating">
+                                <img src="assets/icon/rating-icon.png" alt="">
+                                <p class="text-sedang">4.9</p>
+                            </div>
+                            <div class="text-right btn-card">
+                                <a href="#" class="btn btn-primary text-biasa">Pesan</a>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+
+            </section>
+            <section class="col-lg-4">
+                <a href="#" class="ahref-no-decor second-text-color ">
+                    <div class="card border-image">
+                        <img src="assets/img/raja-ampat.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Raja Ampat</h5>
+                            <div class="pulau">
+                                <p class="card-text text-biasa">Papua Barat</p>
+                            </div>
+                            <p class="text-kecil font-12 mt-4 mb-n1">Mulai dari</p>
+                            <p class="text-tebal mb-0">Rp <span class="warna-harga">600.000</span><span
+                                    class="text-biasa kecil">/org</span>
+                            </p>
+                            <div class="rating">
+                                <img src="assets/icon/rating-icon.png" alt="">
+                                <p class="text-sedang">4.9</p>
+                            </div>
+                            <div class="text-right btn-card">
+                                <a href="#" class="btn btn-primary text-biasa">Pesan</a>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+            </section>
+            <section class="col-lg-4">
+                <a href="#" class="ahref-no-decor second-text-color">
+                    <div class="card border-image">
+                        <img src="assets/img/bali.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bali</h5>
+                            <div class="pulau">
+                                <p class="card-text text-biasa">Bali</p>
+                            </div>
+                            <p class="text-kecil font-12 mt-4 mb-n1">Mulai dari</p>
+                            <p class="text-tebal mb-0">Rp <span class="warna-harga">600.000</span><span
+                                    class="text-biasa kecil">/org</span>
+                            </p>
+                            <div class="rating">
+                                <img src="assets/icon/rating-icon.png" alt="">
+                                <p class="text-sedang">4.9</p>
+                            </div>
+                            <div class="text-right btn-card">
+                                <a href="#" class="btn btn-primary text-biasa">Pesan</a>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+            </section>
+        </section>
+
+        <section class="row">
+            <section class="col-lg-4">
+                <a href="#" class="ahref-no-decor second-text-color ">
+                    <div class="card">
+                        <div class="border-image">
+                            <img src="assets/img/bunaken.png" class="card-img-top" alt="..."></div>
+                        <div class="card-body">
+                            <h5 class="card-title">Taman laut bunaken</h5>
+                            <div class="pulau">
+                                <p class="card-text text-biasa">Sulawesi Utara</p>
+                            </div>
+                            <p class="text-kecil font-12 mt-4 mb-n1">Mulai dari</p>
+                            <p class="text-tebal mb-0">Rp <span class="warna-harga">600.000</span><span
+                                    class="text-biasa kecil">/org</span>
+                            </p>
+                            <div class="rating">
+                                <img src="assets/icon/rating-icon.png" alt="">
+                                <p class="text-sedang">4.9</p>
+                            </div>
+                            <div class="text-right btn-card">
+                                <a href="#" class="btn btn-primary text-biasa">Pesan</a>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+            </section>
+            <section class="col-lg-4">
+                <a href="#" class="ahref-no-decor second-text-color ">
+                    <div class="card border-image">
+                        <img src="assets/img/danau-toba.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Danau Toba</h5>
+                            <div class="pulau">
+                                <p class="card-text text-biasa">Sumatera Utara</p>
+                            </div>
+                            <p class="text-kecil font-12 mt-4 mb-n1">Mulai dari</p>
+                            <p class="text-tebal mb-0">Rp <span class="warna-harga">600.000</span><span
+                                    class="text-biasa kecil">/org</span>
+                            </p>
+                            <div class="rating">
+                                <img src="assets/icon/rating-icon.png" alt="">
+                                <p class="text-sedang">4.9</p>
+                            </div>
+                            <div class="text-right btn-card">
+                                <a href="#" class="btn btn-primary text-biasa">Pesan</a>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+            </section>
+            <section class="col-lg-4">
+                <a href="#" class="ahref-no-decor second-text-color ">
+                    <div class="card border-image">
+                        <img src="assets/img/danau-kelimutu.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Danau Kelimutu</h5>
+                            <div class="pulau">
+                                <p class="card-text text-biasa">Nusa Tenggara Timur</p>
+                            </div>
+                            <p class="text-kecil font-12 mt-4 mb-n1">Mulai dari</p>
+                            <p class="text-tebal mb-0">Rp <span class="warna-harga">600.000</span><span
+                                    class="text-biasa kecil">/org</span>
+                            </p>
+                            <div class="rating">
+                                <img src="assets/icon/rating-icon.png" alt="">
+                                <p class="text-sedang">4.9</p>
+                            </div>
+                            <div class="text-right btn-card">
+                                <a href="#" class="btn btn-primary text-biasa">Pesan</a>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+            </section>
+        </section>
+
+        <!-- FOOTER -->
+        <section class="row margin-footer">
+            <section class="col-lg-4">
+                <img src="assets/img/logo.png" class="logo-footer" alt="">
+                <p class="accent-text-color text-tebal font-18">2021 © Copyright</p>
+            </section>
+            <section class="col-lg-2 mt-2">
+                <div class="cont-footer">
+                    <a href="#" class="border-footer accent-text-color text-biasa font-16">Beranda</a>
+                </div>
+                <div class="cont-footer-text-panjang mt-4">
+                    <a href="#" class="border-footer accent-text-color text-biasa font-16">Kontak<span
+                            class="opacity-0">..</span>kami</a>
+                </div>
+            </section>
+            <section class="col-lg-2 mt-2">
+                <div class="cont-footer">
+                    <a href="#" class="border-footer accent-text-color text-biasa font-16">Galeri</a>
+                </div>
+                <div class="cont-footer-text-panjang mt-4">
+                    <a href="#" class="border-footer accent-text-color text-biasa font-16">Tentang<span
+                            class="opacity-0">..</span>kami</a>
+                </div>
+            </section>
+            <section class="col-lg-4 mt-2">
+                <p class="font-16 text-sedang">Ikuti kami</p>
+                <a href="#" class="ahref-no-decor">
+                    <img src="assets/icon/fb.png" alt="" class="icon-footer">
+                </a>
+                <a href="https://www.instagram.com/gya.af/" target="blank" class="ahref-no-decor">
+                    <img src="assets/icon/ig.png" alt="" class="icon-footer">
+                </a>
+
+            </section>
+        </section>
+
+        <hr class="accent-box-color mt-5">
+    </div>
+
+    <!-- //*CONTENT -->
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
