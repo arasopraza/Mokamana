85d2d771bd14ba92d1db6955fc7f8b33649f3041
arasopraza <aras.opraza@gmail.com>
2021-01-18 02:29:55 +0700

    Add register and login

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- my css -->
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/template.css') }}">
+
+
+    <title>Mokamana</title>
+</head>
+
+<body>
+    <section class="container-fluid">
+        <section class="row justify-content-center">
+
+            <section class="col-lg-7" style="padding: 0;">
+                <div class="img-box">
+                    <img src="assets/img/login1.png" style="border: 0;" alt="" class="img-thumbnail">
+                    <p class="text-left p-atas">Kerja mulu,<br>sekali-kali liburan dong..</p>
+                    <!-- <div class="location-box">
+
+                    </div> -->
+                    <div class="location-box">Labuan Bajo</div>
+                </div>
+            </section>
+
+
+            <section class="col-lg-5">
+                <form method="POST" action="{{ route('register') }}" class="form-container-regist">
+                @csrf
+                    <div class="text-center atas-form">
+                        <img src="assets/img/logo.png" alt="">
+                        <p>Selamat datang</p>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Username</label>
+                        <input id="name" type="text" class="form-control" name="name" aria-describedby="emailHelp" required autofocus>
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">E-mail Address</label>
+                        <input id="email" type="email" class="form-control" name="email" aria-describedby="emailHelp" required>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input id="password" type="password" class="form-control" name="password" required>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="password-confirm">Password</label>
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Ingat saya</label>
+                    </div>
+                    <button type="submit" class="btn btn-block">Daftar</button>
+                    <p>Sudah punya akun? <a href="login.html">Masuk disini</a></p>
+                </form>
+
+            </section>
+        </section>
+    </section>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
