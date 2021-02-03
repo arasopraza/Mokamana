5f31b24bdf1ac5bb2808751efe65a0092c9a74fe
Gissart <argyaauliaf@gmail.com>
2021-02-04 00:35:29 +0700

    fixed hotelcard size on hover, TAPI MASIH BUG

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -27,8 +27,8 @@ class HotelController extends Controller
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_HTTPHEADER => [
                 "x-rapidapi-host: hotels4.p.rapidapi.com",
-                "x-rapidapi-key: 2327f47d46mshd2bfb92db0d6e4dp11cb9ejsn2a667a0c4152"
-            ],
+                "x-rapidapi-key: ba7ebb0ec3msh0c6ba67d0becbe9p1359f6jsn07de4881b75d"
+            ],  
         ]);
         $response = curl_exec($curl);
         $err = curl_error($curl);
@@ -44,13 +44,13 @@ class HotelController extends Controller
         $cityName = $request->input('kota');
         $this->validate($request, ['kota' => 'required']);
 
-        $getHotel = $this->get_CURL("https://hotels4.p.rapidapi.com/locations/search?query=$cityName&locale=en_US");
+        $getHotel = $this->get_CURL("https://hotels4.p.rapidapi.com/locations/search?query=$cityName&locale=en_ID");
         $responseHotel = $getHotel["suggestions"][1]["entities"];
 
         foreach ($responseHotel as $items) {
             $id = $items["destinationId"];
             $getPhotoHotel = $this->get_CURL("https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id=$id");
-            $getDetailHotel = $this->get_CURL("https://hotels4.p.rapidapi.com/properties/get-details?id=$id&locale=en_US&currency=USD&checkOut=2020-01-15&adults1=1&checkIn=2020-01-08");
+            $getDetailHotel = $this->get_CURL("https://hotels4.p.rapidapi.com/properties/get-details?id=$id&locale=en_ID&currency=USD&checkOut=2020-01-15&adults1=1&checkIn=2020-01-08");
 
             $nameHotel = $items["name"];
             $ratingHotel = $getDetailHotel["data"]["body"]["propertyDescription"]["starRating"];
