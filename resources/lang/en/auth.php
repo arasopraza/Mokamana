89cc04c57e8903913d6b43382e93bc33c58e7c62
arasopraza <aras.opraza@gmail.com>
2021-02-18 01:07:43 +0700

    Add login and register custom message

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'E-mail atau password salah.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
